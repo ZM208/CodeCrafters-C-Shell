10d42ad308b015381803aa6acd28fc1ba43ab5fa
Mason Elliss <m.elliss@talktalk.net>
2025-01-05 01:24:05 +0000

    fixes to csprojectfile and exits on command

--- src/main.cs
+++ src/main.cs
@@ -1,12 +1,16 @@
 using System.Net;
 using System.Net.Sockets;
-
+using System;
 // Uncomment this line to pass the first stage
 
 // Wait for user input
 while (true)
 {
     Console.Write("$ ");
-    string userInput = Console.ReadLine();
+    string userInput = Console.ReadLine() ?? "";
     Console.Write($"{userInput}: command not found\n");
+    if (userInput.Contains("exit"))
+    {
+        Environment.Exit(0);
+    }
 }
