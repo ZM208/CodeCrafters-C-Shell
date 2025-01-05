079ee96bf428e32e5ff6ea614e24f5b1453f66a7
Mason Elliss <m.elliss@talktalk.net>
2025-01-05 01:10:48 +0000

    allow multiple inputs

--- src/main.cs
+++ src/main.cs
@@ -2,8 +2,11 @@ using System.Net;
 using System.Net.Sockets;
 
 // Uncomment this line to pass the first stage
-Console.Write("$ ");
 
 // Wait for user input
-string userInput = Console.ReadLine();
-Console.Write($"{userInput}: command not found\n");
+while (true)
+{
+    Console.Write("$ ");
+    string userInput = Console.ReadLine();
+    Console.Write($"{userInput}: command not found\n");
+}
