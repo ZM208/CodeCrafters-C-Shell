b19542a1b269bb5a81edd3b444f2e6de8579ea58
ZM208 <m.elliss@talktalk.net>
2025-01-06 19:18:08 +0000

    echo echo

--- src/main.cs
+++ src/main.cs
@@ -8,9 +8,25 @@ while (true)
 {
     Console.Write("$ ");
     string userInput = Console.ReadLine() ?? "";
-    if (userInput.Contains("exit"))
+    string command = userInput.Split(' ')[0];
+
+    switch (command)
     {
-        Environment.Exit(0);
+        case "echo":
+            {
+                var text = userInput.Replace("echo ", "");
+                Console.Write(text + "\n");
+                break;
+            }
+        case "exit":
+            {
+                Environment.Exit(0);
+                break;
+            }
+        default:
+            {
+                Console.Write($"{userInput}: command not found\n");
+                break;
+            }
     }
-    Console.Write($"{userInput}: command not found\n");
 }
