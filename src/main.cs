521bc8f869753366252260c500861397f52d9a79
ZM208 <m.elliss@talktalk.net>
2025-03-05 21:13:42 +0000

    write again

--- src/main.cs
+++ src/main.cs
@@ -15,7 +15,7 @@ string WorkingDirectory = Environment.CurrentDirectory;
 
 while (true)
 {
-    Console.Write("$ ");
+    Console.Write("$ "); 
     List<string> userInput = HandleUserInput(Console.ReadLine() ?? "");
     string command = userInput[0];
     switch (command)
