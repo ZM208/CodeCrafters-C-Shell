4a77868f0ef9b37f1507d9e78dac980245e78734
ZM208 <m.elliss@talktalk.net>
2025-04-03 21:48:43 +0100

    attempting to fix test bu

--- src/main.cs
+++ src/main.cs
@@ -225,6 +225,6 @@ void StartProcess(string fileName, string args, StreamWriter stream = null)
     process.Start();
     process.BeginOutputReadLine();
     process.WaitForExit();
-    Console.Write(output.ToString().Trim());
+    Console.Write(output.ToString());
     return;
 }
