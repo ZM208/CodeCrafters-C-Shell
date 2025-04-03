aca4ac3f33540c75236544b2bb82f6aab72801e7
ZM208 <m.elliss@talktalk.net>
2025-04-03 21:45:43 +0100

    test

--- src/main.cs
+++ src/main.cs
@@ -222,6 +222,7 @@ void StartProcess(string fileName, string args, StreamWriter stream = null)
     {
         process.OutputDataReceived += (_, dataReceived) => output.AppendLine(dataReceived.Data);
     }
+    Console.WriteLine(output);
     process.Start();
     process.BeginOutputReadLine();
     Console.Write(output.ToString());
