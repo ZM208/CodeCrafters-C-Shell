02fc3fbe784b5b6f2b6e03cbf9573086dfec48eb
ZM208 <m.elliss@talktalk.net>
2025-04-06 21:20:12 +0100

    test

--- src/main.cs
+++ src/main.cs
@@ -65,7 +65,7 @@ while (true)
                 userInput.RemoveAt(0);
                 userInput = userInput.Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
                 var allContent = userInput.Select(File.ReadAllText);
-                Console.WriteLine(string.Join("", allContent));
+                Console.WriteLine(string.Join("", allContent).Trim());
                 break; 
             }
         default:
